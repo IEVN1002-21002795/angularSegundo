e3c26e421b9f54df3b75103a4e621433608a697d
Diego Armando Hernandez Trujillo <diegotrujillo1899@gamil.com>
2024-10-12 10:26:48 -0600

    Commit Rutas

---
+++ src/app/auth/features/auth.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from "@angular/router";
+
+export default[
+    {
+        path: 'sing-in',
+        loadComponent:()=> import('./sing-in/sing-in.component'),
+
+    },
+    {
+        path: 'sing-up',
+        loadComponent:()=> import('./sing-up/sing-up.component'),
+    },
+    {
+        path: '*',
+        redirectTo: ''
+    }
+]as Routes
